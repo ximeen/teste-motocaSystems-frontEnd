934671f3d881148fbb73e863729ce8334470ee1d
Gabriel Ximenes <swg-ximenes@iMac-Pro-de-Gabriel.local>
2024-06-14 19:24:25 -0300

    :up: update queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/globals.css'
 import { App } from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient.ts'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
